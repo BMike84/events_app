81cf2e56d2d73eb629a8ec70968ab73841351727
Michael Buffone <m_buffone@hotmail.com>
2022-12-28 09:11:40 -0500

    started learning how to make seperate pages and added data

---
+++ events_app/pages/events/index.js
@@ -0,0 +1,23 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events Page</h1>
+      <div>
+        <a href="">
+          <img />
+          <h2>Events In London</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events In San Franciso</h2>
+        </a>
+        <a href="">
+          <img />
+          <h2>Events In Barcelona</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
